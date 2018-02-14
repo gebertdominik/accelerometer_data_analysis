2ddbb0338ea4461305bbb2550141c6bd1d00ca37
dgebert <dgebert91@gmail.com>
2018-02-14 16:45:26 +0100

    Fix filepaths

--- statistics.py
+++ statistics.py
@@ -36,7 +36,7 @@ RUNNING_PERSON_1['magnitude'] = sig.magnitude(RUNNING_PERSON_1)
 """ PERSON_1 - prepare data - stop """
 
 activities_person_1 = [STANDING_PERSON_1, WALKING_PERSON_1, DOWNSTAIRS_PERSON_1, UPSTAIRS_PERSON_1, RUNNING_PERSON_1]
-output_file_path_person_1 = 'mgr/data/resources/person_1/Features.csv'
+output_file_path_person_1 = 'mgr/data/features/person_1/Features.csv'
 
 with open(output_file_path_person_1, 'w') as features_file:
     rows = csv.writer(features_file)
@@ -44,7 +44,7 @@ with open(output_file_path_person_1, 'w') as features_file:
         for f in sig.extract_features(activities_person_1[i]):
             rows.writerow([i] + f)
 
-features_person_1 = np.loadtxt('mgr/data/resources/person_1/Features.csv', delimiter=",")
+features_person_1 = np.loadtxt('mgr/data/features/person_1/Features.csv', delimiter=",")
 
 print('\nTest Classifiers on PERSON_1 learned with data collected by PERSON_1')
 print('Dummy Classifier        ', sig.test_dummy_cls_one_set(features_person_1))
@@ -79,7 +79,7 @@ RUNNING_PERSON_2['magnitude'] = sig.magnitude(RUNNING_PERSON_2)
 """ PERSON_2 - prepare data - stop """
 
 activities_person_2 = [STANDING_PERSON_2, WALKING_PERSON_2, DOWNSTAIRS_PERSON_2, UPSTAIRS_PERSON_2, RUNNING_PERSON_2]
-output_file_path_person_2 = 'mgr/data/resources/person_2/Features.csv'
+output_file_path_person_2 = 'mgr/data/features/person_2/Features.csv'
 
 with open(output_file_path_person_2, 'w') as features_file:
     rows = csv.writer(features_file)
@@ -87,7 +87,7 @@ with open(output_file_path_person_2, 'w') as features_file:
         for f in sig.extract_features(activities_person_2[i]):
             rows.writerow([i] + f)
 
-features_person_2 = np.loadtxt('mgr/data/resources/person_2/Features.csv', delimiter=",")
+features_person_2 = np.loadtxt('mgr/data/features/person_2/Features.csv', delimiter=",")
 
 print('\nTest Classifiers on PERSON_2 learned with data collected by PERSON_2')
 print('Dummy Classifier        ', sig.test_dummy_cls_one_set(features_person_2))
@@ -122,7 +122,7 @@ RUNNING_PERSON_3['magnitude'] = sig.magnitude(RUNNING_PERSON_3)
 """ PERSON_3 - prepare data - stop """
 
 activities_person_3 = [STANDING_PERSON_3, WALKING_PERSON_3, DOWNSTAIRS_PERSON_3, UPSTAIRS_PERSON_3, RUNNING_PERSON_3]
-output_file_path_person_3 = 'mgr/data/resources/person_3/Features.csv'
+output_file_path_person_3 = 'mgr/data/features/person_3/Features.csv'
 
 with open(output_file_path_person_3, 'w') as features_file:
     rows = csv.writer(features_file)
@@ -130,7 +130,7 @@ with open(output_file_path_person_3, 'w') as features_file:
         for f in sig.extract_features(activities_person_3[i]):
             rows.writerow([i] + f)
 
-features_person_3 = np.loadtxt('mgr/data/resources/person_3/Features.csv', delimiter=",")
+features_person_3 = np.loadtxt('mgr/data/features/person_3/Features.csv', delimiter=",")
 
 print('\nTest Classifiers on PERSON_3 learned with data collected by PERSON_3')
 print('Dummy Classifier        ', sig.test_dummy_cls_one_set(features_person_3))
@@ -165,7 +165,7 @@ RUNNING_PERSON_4['magnitude'] = sig.magnitude(RUNNING_PERSON_4)
 """ PERSON_4 - prepare data - stop """
 
 activities_person_4 = [STANDING_PERSON_4, WALKING_PERSON_4, DOWNSTAIRS_PERSON_4, UPSTAIRS_PERSON_4, RUNNING_PERSON_4]
-output_file_path_person_4 = 'mgr/data/resources/person_4/Features.csv'
+output_file_path_person_4 = 'mgr/data/features/person_4/Features.csv'
 
 with open(output_file_path_person_4, 'w') as features_file:
     rows = csv.writer(features_file)
@@ -173,7 +173,7 @@ with open(output_file_path_person_4, 'w') as features_file:
         for f in sig.extract_features(activities_person_4[i]):
             rows.writerow([i] + f)
 
-features_person_4 = np.loadtxt('mgr/data/resources/person_4/Features.csv', delimiter=",")
+features_person_4 = np.loadtxt('mgr/data/features/person_4/Features.csv', delimiter=",")
 
 
 print('\nTest Classifiers on PERSON_4 learned with data collected by PERSON_4')
@@ -226,45 +226,3 @@ print('Decision Tree Classifier', sig.test_and_learn_decision_tree_cls(features_
 print('Random Forest Classifier', sig.test_and_learn_random_forest_cls(features_all, features_person_4))
 print('MLP Classifier          ', sig.test_and_learn_mlp_cls(features_all, features_person_4))
 print('GaussianNB              ', sig.test_and_learn_gaussian_nb_cls(features_all, features_person_4))
-
-### Sample
-"""
-activity_features = np.loadtxt('mgr/data/resources/Features_activity.csv', delimiter=",")
-print("Features end")
-print(datetime.datetime.now())
-#DRAW - WORKING
-#data_to_predict = features[:, 1:] #dla danych treningowych
-data_to_predict = activity_features
-print("Prediction start")
-print(datetime.datetime.now())
-y = np.array(random_forest_cls_all.predict(data_to_predict))
-x = np.linspace(0, len(data_to_predict)-1, len(data_to_predict))
-print("Prediction end")
-print(datetime.datetime.now())
-
-data = np.column_stack((x,y))
-
-STANDING_COLOR = 'gray'
-WALKING_COLOR = 'green'
-DOWNSTAIRS_COLOR = 'brown'
-UPSTAIRS_COLOR = 'blue'
-RUNNING_COLOR = 'red'
-
-standing = data[data[:, 1] == 0]
-walking = data[data[:, 1] == 1]
-downstairs = data[data[:, 1] == 2]
-upstairs = data[data[:, 1] == 3]
-running = data[data[:, 1] == 4]
-dot_size = 3
-print("Printing")
-print(datetime.datetime.now())
-plt.scatter(standing[:, 0], standing[:,1], dot_size, c=STANDING_COLOR, label='standing')
-plt.scatter(walking[:, 0], walking[:,1], dot_size, c=WALKING_COLOR, label='walking')
-plt.scatter(downstairs[:, 0], downstairs[:,1], dot_size, c=DOWNSTAIRS_COLOR, label='downstairs')
-plt.scatter(upstairs[:, 0], upstairs[:, 1], dot_size, c=UPSTAIRS_COLOR, label='upstairs')
-plt.scatter(running[:, 0], running[:,1], dot_size, c=RUNNING_COLOR, label='running')
-plt.legend(loc='best')
-print("Show")
-print(datetime.datetime.now())
-plt.show()
-"""
