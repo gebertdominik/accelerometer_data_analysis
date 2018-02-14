e0073b62effb6c106fab750efee0daeca141a125
dgebert <dgebert91@gmail.com>
2018-02-14 16:43:16 +0100

    Remove dummy cls and fix gaussianNB method cal

--- test.py
+++ test.py
@@ -1,11 +1,4 @@
-import matplotlib.pyplot as plt
-from scipy.signal import filtfilt
-from scipy.signal import lfilter, butter
-from scipy.signal import lfilter_zi
 
-import mgr.data.readrawdata as rd
-import mgr.visualization.visualize as vis
-import mgr.calc.signal as sig
 import numpy as np
 from prepare_data import *
 
@@ -13,43 +6,37 @@ features_person_1 = np.loadtxt(features_file_person_1, delimiter=",")
 features_person_2 = np.loadtxt(features_file_person_2, delimiter=",")
 features_person_3 = np.loadtxt(features_file_person_3, delimiter=",")
 features_person_4 = np.loadtxt(features_file_person_4, delimiter=",")
-
-#features_all = np.concatenate([features_person_1, features_person_2, features_person_3, features_person_4])
+features_all = np.concatenate([features_person_1, features_person_2, features_person_3, features_person_4])
 features_person_1_2_3 = np.concatenate([features_person_1, features_person_2, features_person_3])
-#features_person_1_2_4 = np.concatenate([features_person_1, features_person_2, features_person_4])
-#features_person_1_3_4 = np.concatenate([features_person_1, features_person_3, features_person_4])
-#features_person_2_3_4 = np.concatenate([features_person_2, features_person_3, features_person_4])
-
+features_person_1_2_4 = np.concatenate([features_person_1, features_person_2, features_person_4])
+features_person_1_3_4 = np.concatenate([features_person_1, features_person_3, features_person_4])
+features_person_2_3_4 = np.concatenate([features_person_2, features_person_3, features_person_4])
 
 print('\nTest Classifiers - learn: 1,2,3 test: 4')
-# print('Dummy Classifier        ', sig.test_and_learn_dummy_cls(features_person_1_2_3, features_person_4))
 print('K-Neighbors Classifier  ', sig.test_and_learn_knn_cls(features_person_1_2_3, features_person_4))
 print('Decision Tree Classifier', sig.test_and_learn_decision_tree_cls(features_person_1_2_3, features_person_4))
 print('Random Forest Classifier', sig.test_and_learn_random_forest_cls(features_person_1_2_3, features_person_4))
 print('MLP Classifier          ', sig.test_and_learn_mlp_cls(features_person_1_2_3, features_person_4))
-print('GaussianNB              ', sig.test_and_learn_mlp_cls(features_person_1_2_3, features_person_4))
+print('GaussianNB              ', sig.test_and_learn_gaussian_nb_cls(features_person_1_2_3, features_person_4))
 
-"""print('\nTest Classifiers - learn: 1,2,4 test: 3')
-print('Dummy Classifier        ', sig.test_and_learn_dummy_cls(features_person_1_2_4, features_person_3))
+print('\nTest Classifiers - learn: 1,2,4 test: 3')
 print('K-Neighbors Classifier  ', sig.test_and_learn_knn_cls(features_person_1_2_4, features_person_3))
 print('Decision Tree Classifier', sig.test_and_learn_decision_tree_cls(features_person_1_2_4, features_person_3))
 print('Random Forest Classifier', sig.test_and_learn_random_forest_cls(features_person_1_2_4, features_person_3))
 print('MLP Classifier          ', sig.test_and_learn_mlp_cls(features_person_1_2_4, features_person_3))
-print('GaussianNB              ', sig.test_and_learn_mlp_cls(features_person_1_2_4, features_person_3))
+print('GaussianNB              ', sig.test_and_learn_gaussian_nb_cls(features_person_1_2_4, features_person_3))
 
 print('\nTest Classifiers - learn: 1,3,4 test: 2')
-print('Dummy Classifier        ', sig.test_and_learn_dummy_cls(features_person_1_3_4, features_person_2))
 print('K-Neighbors Classifier  ', sig.test_and_learn_knn_cls(features_person_1_3_4, features_person_2))
 print('Decision Tree Classifier', sig.test_and_learn_decision_tree_cls(features_person_1_3_4, features_person_2))
 print('Random Forest Classifier', sig.test_and_learn_random_forest_cls(features_person_1_3_4, features_person_2))
 print('MLP Classifier          ', sig.test_and_learn_mlp_cls(features_person_1_3_4, features_person_2))
-print('GaussianNB              ', sig.test_and_learn_mlp_cls(features_person_1_3_4, features_person_2))
+print('GaussianNB              ', sig.test_and_learn_gaussian_nb_cls(features_person_1_3_4, features_person_2))
 
 print('\nTest Classifiers - learn: 2,3,4 test: 1')
-print('Dummy Classifier        ', sig.test_and_learn_dummy_cls(features_person_2_3_4, features_person_1))
 print('K-Neighbors Classifier  ', sig.test_and_learn_knn_cls(features_person_2_3_4, features_person_1))
 print('Decision Tree Classifier', sig.test_and_learn_decision_tree_cls(features_person_2_3_4, features_person_1))
 print('Random Forest Classifier', sig.test_and_learn_random_forest_cls(features_person_2_3_4, features_person_1))
 print('MLP Classifier          ', sig.test_and_learn_mlp_cls(features_person_2_3_4, features_person_1))
-print('GaussianNB              ', sig.test_and_learn_mlp_cls(features_person_2_3_4, features_person_1))"""
+print('GaussianNB              ', sig.test_and_learn_gaussian_nb_cls(features_person_2_3_4, features_person_1))
 
