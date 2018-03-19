e23dccd122a9866fffa0d6f5f38e5ca7b9a32347
dgebert <dgebert91@gmail.com>
2018-03-19 10:26:06 +0100

    Start activities from timestamp = 0 seconds

--- mgr/data/readrawdata.py
+++ mgr/data/readrawdata.py
@@ -4,6 +4,7 @@ import pandas as pd
 def read(filename):
     columns = ['timestamp', 'xAxis', 'yAxis', 'zAxis']
     data = pd.read_csv(filename, skiprows=300, skipfooter=300, header=None, names=columns)
+    data['timestamp'] = data['timestamp'] - min(data['timestamp'])
     return data
 
 
