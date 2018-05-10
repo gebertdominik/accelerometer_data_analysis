b75ccd5b9d28ce6a4aa5f9f332fdaaca0a52d6f5
dgebert <dgebert91@gmail.com>
2018-05-10 20:08:25 +0200

    Remove filtering

--- prepare_data.py
+++ prepare_data.py
@@ -20,23 +20,18 @@ RUNNING_COLOR = 'red'
 
 """ PERSON_1 - prepare data - start """
 STANDING_PERSON_1 = rd.read('mgr/data/resources/person_1/standing.csv')
-sig.denoise(STANDING_PERSON_1)
 STANDING_PERSON_1['magnitude'] = sig.magnitude(STANDING_PERSON_1)
 
 WALKING_PERSON_1 = rd.read('mgr/data/resources/person_1/walking.csv')
-sig.denoise(WALKING_PERSON_1)
 WALKING_PERSON_1['magnitude'] = sig.magnitude(WALKING_PERSON_1)
 
 DOWNSTAIRS_PERSON_1 = rd.read('mgr/data/resources/person_1/downstairs.csv')
-sig.denoise(DOWNSTAIRS_PERSON_1)
 DOWNSTAIRS_PERSON_1['magnitude'] = sig.magnitude(DOWNSTAIRS_PERSON_1)
 
 UPSTAIRS_PERSON_1 = rd.read('mgr/data/resources/person_1/upstairs.csv')
-sig.denoise(UPSTAIRS_PERSON_1)
 UPSTAIRS_PERSON_1['magnitude'] = sig.magnitude(UPSTAIRS_PERSON_1)
 
 RUNNING_PERSON_1 = rd.read('mgr/data/resources/person_1/running.csv')
-sig.denoise(RUNNING_PERSON_1)
 RUNNING_PERSON_1['magnitude'] = sig.magnitude(RUNNING_PERSON_1)
 
 activities_person_1 = [STANDING_PERSON_1, WALKING_PERSON_1, DOWNSTAIRS_PERSON_1, UPSTAIRS_PERSON_1, RUNNING_PERSON_1]
@@ -52,23 +47,18 @@ with open(features_file_person_1, 'w') as features_file:
 """ PERSON_2 - prepare data - start """
 
 STANDING_PERSON_2 = rd.read('mgr/data/resources/person_2/standing.csv')
-sig.denoise(STANDING_PERSON_2)
 STANDING_PERSON_2['magnitude'] = sig.magnitude(STANDING_PERSON_2)
 
 WALKING_PERSON_2 = rd.read('mgr/data/resources/person_2/walking.csv')
-sig.denoise(WALKING_PERSON_2)
 WALKING_PERSON_2['magnitude'] = sig.magnitude(WALKING_PERSON_2)
 
 DOWNSTAIRS_PERSON_2 = rd.read('mgr/data/resources/person_2/downstairs.csv')
-sig.denoise(DOWNSTAIRS_PERSON_2)
 DOWNSTAIRS_PERSON_2['magnitude'] = sig.magnitude(DOWNSTAIRS_PERSON_2)
 
 UPSTAIRS_PERSON_2 = rd.read('mgr/data/resources/person_2/upstairs.csv')
-sig.denoise(UPSTAIRS_PERSON_2)
 UPSTAIRS_PERSON_2['magnitude'] = sig.magnitude(UPSTAIRS_PERSON_2)
 
 RUNNING_PERSON_2 = rd.read('mgr/data/resources/person_2/running.csv')
-sig.denoise(RUNNING_PERSON_2)
 RUNNING_PERSON_2['magnitude'] = sig.magnitude(RUNNING_PERSON_2)
 
 activities_person_2 = [STANDING_PERSON_2, WALKING_PERSON_2, DOWNSTAIRS_PERSON_2, UPSTAIRS_PERSON_2, RUNNING_PERSON_2]
@@ -84,23 +74,18 @@ with open(features_file_person_2, 'w') as features_file:
 """ PERSON_3 - prepare data - start """
 
 STANDING_PERSON_3 = rd.read('mgr/data/resources/person_3/standing.csv')
-sig.denoise(STANDING_PERSON_3)
 STANDING_PERSON_3['magnitude'] = sig.magnitude(STANDING_PERSON_3)
 
 WALKING_PERSON_3 = rd.read('mgr/data/resources/person_3/walking.csv')
-sig.denoise(WALKING_PERSON_3)
 WALKING_PERSON_3['magnitude'] = sig.magnitude(WALKING_PERSON_3)
 
 DOWNSTAIRS_PERSON_3 = rd.read('mgr/data/resources/person_3/downstairs.csv')
-sig.denoise(DOWNSTAIRS_PERSON_3)
 DOWNSTAIRS_PERSON_3['magnitude'] = sig.magnitude(DOWNSTAIRS_PERSON_3)
 
 UPSTAIRS_PERSON_3 = rd.read('mgr/data/resources/person_3/upstairs.csv')
-sig.denoise(UPSTAIRS_PERSON_3)
 UPSTAIRS_PERSON_3['magnitude'] = sig.magnitude(UPSTAIRS_PERSON_3)
 
 RUNNING_PERSON_3 = rd.read('mgr/data/resources/person_3/running.csv')
-sig.denoise(RUNNING_PERSON_3)
 RUNNING_PERSON_3['magnitude'] = sig.magnitude(RUNNING_PERSON_3)
 
 activities_person_3 = [STANDING_PERSON_3, WALKING_PERSON_3, DOWNSTAIRS_PERSON_3, UPSTAIRS_PERSON_3, RUNNING_PERSON_3]
@@ -116,23 +101,18 @@ with open(features_file_person_3, 'w') as features_file:
 """ PERSON_4 - prepare data - start """
 
 STANDING_PERSON_4 = rd.read('mgr/data/resources/person_4/standing.csv')
-sig.denoise(STANDING_PERSON_4)
 STANDING_PERSON_4['magnitude'] = sig.magnitude(STANDING_PERSON_4)
 
 WALKING_PERSON_4 = rd.read('mgr/data/resources/person_4/walking.csv')
-sig.denoise(WALKING_PERSON_4)
 WALKING_PERSON_4['magnitude'] = sig.magnitude(WALKING_PERSON_4)
 
 DOWNSTAIRS_PERSON_4 = rd.read('mgr/data/resources/person_4/downstairs.csv')
-sig.denoise(DOWNSTAIRS_PERSON_4)
 DOWNSTAIRS_PERSON_4['magnitude'] = sig.magnitude(DOWNSTAIRS_PERSON_4)
 
 UPSTAIRS_PERSON_4 = rd.read('mgr/data/resources/person_4/upstairs.csv')
-sig.denoise(UPSTAIRS_PERSON_4)
 UPSTAIRS_PERSON_4['magnitude'] = sig.magnitude(UPSTAIRS_PERSON_4)
 
 RUNNING_PERSON_4 = rd.read('mgr/data/resources/person_4/running.csv')
-sig.denoise(RUNNING_PERSON_4)
 RUNNING_PERSON_4['magnitude'] = sig.magnitude(RUNNING_PERSON_4)
 
 """ PERSON_4 - prepare data - stop """
